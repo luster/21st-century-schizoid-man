6b47c0f2dc074763bcfd7c240be7f8b6026b2744
jastrauckas <jastrauckas@gmail.com>
2014-06-26 23:14:02 -0400

    adding new RVM that gets 86 percent with 5 features

---
+++ matlab/keene_rvm2.m
@@ -0,0 +1,41 @@
+% main - keene
+
+clear all;clc; close all
+prtPath('alpha','beta')
+
+%% load training data
+
+load training.mat
+
+train_features = [train_sbm,train_fnc]; % concatenate features
+
+ds = prtDataSetClass(train_features, train_labels);
+
+%% try some feature selection shit
+% LLN
+% doing this gives 86%, might be more stable than full features
+featSel = prtFeatSelLlnn('verbosePlot',true);  % Create the feature
+% selection object.
+featSel.nMaxIterations = 10;                   % Set the max # of
+% iterations.
+featSel = featSel.train(ds);              % Train
+featSel. weightChangeThreshold = .1;
+features = featSel.selectedFeatures; % 7 11 24 26 276
+
+ds = featSel.run(ds);
+
+%%
+
+rvm = prtClassRvm;
+rvm.kernels.kernelCell{2}.sigma = .75; % .75 give 88%
+alg =     rvm;
+
+out = alg.kfolds(ds);
+
+figure;
+prtScoreAuc(out)
+
+prtScoreRoc(out)
+
+
+
--- matlab/main.m
+++ matlab/main.m
@@ -18,39 +18,43 @@ load training.mat
 % bigml.com
 bigmlfeatures = [326, 296, 79, 28, 220, 288, 390] + 1;
 
-classifier = prtClassLibSvm;
-
 train_features = [train_sbm,train_fnc]; % concatenate features
 
 ds = prtDataSetClass(train_features, train_labels);
-zmuv = prtPreProcZmuv;
-zmuv = zmuv.train(ds);
-ds = zmuv.run(ds);
+% zmuv = prtPreProcZmuv;
+% zmuv = zmuv.train(ds);
+% ds = zmuv.run(ds);
+
 
 % feature selection
-featSel = prtFeatSelSfs;
-featSel.evaluationMetric = @(DS)prtEvalAuc(classifier,DS);
-featSel.nFeatures = 3;
+% featSel = prtFeatSelSfs;
+% featSel.evaluationMetric = @(DS)prtEvalAuc(classifier,DS);
+% featSel.nFeatures = 3;
 % featSel = prtFeatSelStatic;
 % featSel.selectedFeatures = bigmlfeatures;
 
-featSel = featSel.train(ds);
-dsFeatSel = featSel.run(ds);
+% featSel = featSel.train(ds);
+% dsFeatSel = featSel.run(ds);
+dsFeatSel = ds;
 
 %% train classifier
 
 % preprocessing
 zmuv = prtPreProcZmuv;
 
+%classifier = prtClassLibSvm;
+classifier = prtClassRvm;
+classifier.kernels.kernelCell{2}.sigma = .75; % .75 give 88%
 clf = classifier;
 clf.internalDecider = prtDecisionBinaryMinPe;
-alg = zmuv + clf;
+%alg = zmuv + clf;
+alg = clf;
 
 alg = alg.train(dsFeatSel);
 
 %% cross-validate using leave-one-out
 
-result = alg.kfolds(dsFeatSel, length(dsFeatSel.targets));
+result = alg.kfolds(dsFeatSel);
 
 %% results
 
